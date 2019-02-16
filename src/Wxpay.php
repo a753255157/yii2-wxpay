a2b5b068cf8fb7bfcf1cc07d78701f0d642ebaf4
lkw <853255156@qq.com>
2019-02-16 19:03:22 +0800

    add wx applet pay support

--- src/Wxpay.php
+++ src/Wxpay.php
@@ -1,12 +1,12 @@
 <?php
 namespace xplwechat\weixin;
 use xplwechat\weixin\lib\WxPayApi;
-use xplwechat\weixin\lib\WxPayDataBase;
 use xplwechat\weixin\lib\WxPayNotify;
 use xplwechat\weixin\lib\WxPayUnifiedOrder;
 use yii\base\Component;
 use yii\base\Exception;
 use yii\base\InvalidConfigException;
+use yii\web\HttpException;
 
 class Wxpay extends Component {
 
@@ -49,33 +49,77 @@ class Wxpay extends Component {
         }
     }
 
+    /**
+     * 统一下单，向微信服务器获取prepay_id
+     * $goods = [
+     *      'tag'           => '',
+     *      'body'          => '',
+     *      'price'         => '',
+     *      'attach'        => '',
+     *      'out_trade_no'  => '',
+     *      'trade_type'    => 'JSAPI' // or 'NATIVE'
+     *      
+     *      // if 'trade_type'=>'JSAPI'
+     *      'openid'        => '',
+     *      // if 'trade_type'=>'NATIVE'
+     *      'product_id'    => ''
+     * ];
+     */
     public function unifiedOrder($goods){
         $input = new WxPayUnifiedOrder();
         $input->SetBody($goods['body']);
         $input->SetAttach($goods['attach']);
-        $input->SetOut_trade_no($this->mch_id.date("YmdHis"));
+        $input->SetOut_trade_no($goods['out_trade_no']);
         $input->SetTotal_fee($goods['price']);
         $input->SetTime_start(date("YmdHis"));
         $input->SetTime_expire(date("YmdHis", time() + 600));
         $input->SetGoods_tag($goods['tag']);
         $input->SetNotify_url($this->back_url);
-        $input->SetTrade_type("APP");
-        $input->SetProduct_id($goods['id']);
-
+        $input->SetTrade_type($goods['trade_type']);
+        
+        if ($input->GetTrade_type() == 'JSAPI'){
+            $input->SetOpenid($goods['openid']);
+        }elseif ($input->GetTrade_type() == 'NATIVE'){
+            $input->SetProduct_id($goods['product_id']);
+        }
+        
         $result = WxPayApi::unifiedOrder($input);
         if($result['return_code'] == 'SUCCESS'){
-            $input = new WxPayUnifiedOrder();
-            $input->SetPublicValue('appid',$result['appid']);
-            $input->SetPublicValue('noncestr',$result['nonce_str']);
-            $input->SetPublicValue('package','Sign=WXPay');
-            $input->SetPublicValue('partnerid',$result['mch_id']);
-            $input->SetPublicValue('prepayid',$result['prepay_id']);
-            $input->SetPublicValue('timestamp',time());
-            $result['sign'] = $input->MakeSign();
-            $result['timestamp'] = (string)time();
             return $result;
+        }else{
+            throw new HttpException(500, $result['return_msg']);
         }
     }
+    
+    //获取prepay_id后，调起微信小程序支付
+    public static function callAppletPay($result)
+    {
+        $input = new WxPayUnifiedOrder();
+        $input->SetPublicValue('appId',$result['appid']);
+        $input->SetPublicValue('nonceStr',$result['nonce_str']);
+        $input->SetPublicValue('package','prepay_id='.$result['prepay_id']);
+        $input->SetPublicValue('timeStamp',(string)time());
+        $input->SetPublicValue('signType', 'MD5');
+        $input->SetPublicValue('paySign', $input->MakeSign());
+        return $input->GetValues();
+    }
+    
+    //获取prepay_id后，调起APP支付
+    public static function callAppPay($result)
+    {
+        $input = new WxPayUnifiedOrder();
+        $input->SetPublicValue('appid',$result['appid']);
+        $input->SetPublicValue('noncestr',$result['nonce_str']);
+        $input->SetPublicValue('package','Sign=WXPay');
+        $input->SetPublicValue('partnerid',$result['mch_id']);
+        $input->SetPublicValue('prepayid',$result['prepay_id']);
+        $input->SetPublicValue('timestamp',time());
+        $result['sign'] = $input->MakeSign();
+        $result['timestamp'] = (string)time();
+        return $result;
+    }
+    
+    //告知微信服务器已收到回调通知
     public function callBackResult($result='SUCCESS',$msg='OK'){
         $input = new WxPayUnifiedOrder();
         $input->SetPublicValue('return_code',$result);
